c435369e7f5cb1fe432a5f6387d65e97353d9705
issaice <hawo1551issa@gmail.com>
2022-06-29 20:21:01 -0700

    4 core features completed, Skeleton set up

---
+++ lifetracker-api/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+// const { PORT } = require("./config");
+// const authRoutes = require("./routes/auth");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("tiny"));
+
+const PORT = process.env.PORT || 3001;
+
+// app.use("/auth", authRoutes);
+
+app.use((req, res, next) => {
+   return next(new NotFoundError());
+});
+
+app.use((error, req, res, next) => {
+   const status = error.status || 500;
+   const message = error.message;
+   return res.status(status).json({
+      error: {message, status}
+   });
+});
+
+app.listen(PORT, () => {
+   console.log(`🥳Server running on http://localhost:${PORT}`);
+})
